edd55ad2470b56733a69a7ac0c0354470c0a121d
Mike Shiner <michael.shiner@siemens.com>
2020-03-31 14:10:45 +0100

    Add SSL cert loading

--- pwa-serve/app.js
+++ pwa-serve/app.js
@@ -1,11 +1,24 @@
 const express = require('express');
 const app = express();
 const port = 8080;
+const https = require('https');
+const http = require('http');
 
 app.use(express.static('dist'));
-
 app.get('*', (req, res) => {
   res.sendFile(path.resolve('dist/index.html'));
 });
 
-app.listen(port, () => console.log(`Example app listening on port ${port}!`));
+http.createServer(app).listen(80);
+
+if (process.env.NODE_ENV === 'prod') {
+  https
+    .createServer(
+      {
+        key: fs.readFileSync('certs/privkey.pem'),
+        cert: fs.readFileSync('certs/cert.pem'),
+      },
+      app
+    )
+    .listen(443);
+}
