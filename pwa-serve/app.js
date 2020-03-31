542f5db4bb2a4138771407be6b6ac160fb779090
Mike Shiner <michael.shiner@siemens.com>
2020-03-31 20:13:11 +0100

    Add certificate configuration options

--- pwa-serve/app.js
+++ pwa-serve/app.js
@@ -1,25 +1,32 @@
 const express = require('express');
 const app = express();
-const port = 8080;
 const https = require('https');
 const http = require('http');
 const fs = require('fs');
+const port = process.env.httpPort || 80;
+const sslPort = process.env.sslPort || 443;
 
 app.use(express.static('dist'));
 app.get('*', (req, res) => {
   res.sendFile(path.resolve('dist/index.html'));
 });
 
-http.createServer(app).listen(80);
+http.createServer(app).listen(port, err => {
+  console.log('Started HTTP Server.');
+  if (err) console.log(err);
+});
 
 if (process.env.NODE_ENV === 'prod') {
   https
     .createServer(
       {
-        key: fs.readFileSync('certs/privkey.pem'),
-        cert: fs.readFileSync('certs/cert.pem'),
+        key: fs.readFileSync('certs/' + process.env.keyName),
+        cert: fs.readFileSync('certs/' + process.env.certName),
       },
       app
     )
-    .listen(443);
+    .listen(sslPort, err => {
+      console.log('Started HTTPS Server.');
+      if (err) console.log(err);
+    });
 }
